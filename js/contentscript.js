6760f14494f8f5a337c6971027d5d16fcedb1785
Cari Davidson <cdavidson@wayfair.com>
2015-06-16 11:37:46 -0400

    instead of adding styles to elements, we are now only adding classes, and there is a dynamically-generated stylesheet.  It's cleaner, and it also allows us to more easily affect sub-elements.

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,4 +1,22 @@
 /**
+ * Generates the style sheet that we need to use
+ */
+function generateStyleSheet(colors) {
+  var styleEl = document.getElementById('nfe_style');
+  if (!styleEl) {
+    styleEl = document.createElement('style');
+    styleEl.type = 'text/css';
+    styleEl.id = 'nfe_style';
+    document.getElementsByTagName('head')[0].appendChild(styleEl);
+    styleEl.appendChild(document.createTextNode('a.nfe-extension-link.nfe-nofollow, a.nfe-extension-link.nfe-nofollow * {color: #' + colors.nofollow_fg + '; background-color: #' + colors.nofollow_bg+ ' !important;}'));
+    styleEl.appendChild(document.createTextNode('a.nfe-extension-link.nfe-nofollow img {border: 6px solid #' + colors.nofollow_bg + ' !important;}'));
+    styleEl.appendChild(document.createTextNode('a.nfe-extension-link.nfe-follow, a.nfe-extension-link.nfe-follow * {color: #' + colors.follow_fg + '; background-color: #' + colors.follow_bg+ ' !important;}'));
+    styleEl.appendChild(document.createTextNode('a.nfe-extension-link.nfe-follow img {border: 6px solid #' + colors.follow_bg + ' !important;}'));
+  }
+
+}
+
+/**
  * Applies Nofollow Highlighter classes
  */
 function applyClasses(domains, colors) {
@@ -9,18 +27,15 @@ function applyClasses(domains, colors) {
       var matches = $el.attr('href').match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
       var domain = matches && matches[1];
       if (domain) {
+        generateStyleSheet(colors);
         for (var i = 0; i < domains.length; i++) {
           // only root domains will be in the settings. Check that host ends with the domain
           if (domain.toLowerCase().indexOf(domains[i].toLowerCase(), domain.length - domains[i].length) !== -1) {
             $el.addClass('nfe-extension-link');
             if ($el.attr("rel") && $el.attr("rel").split(' ').indexOf("nofollow") > -1) {
-              $el.css('background-color', '#' + colors.nofollow_bg);
-              $el.css('color', '#' + colors.nofollow_fg);
-              $el.find('img').css('border', '6px solid #' + colors.nofollow_bg);
+              $el.addClass('nfe-nofollow');
             } else {
-              $el.css('background-color', '#' + colors.follow_bg);
-              $el.css('color', '#' + colors.follow_fg);
-              $el.find('img').css('border', '6px solid #' + colors.follow_bg);
+              $el.addClass('nfe-follow');
             }
             break;
           }
@@ -36,11 +51,8 @@ function applyClasses(domains, colors) {
 function removeClasses() {
   var links = $('.nfe-extension-link');
   links.removeClass('nfe-extension-link');
-  links.css('background-color', '');
-  links.css('color', '');
-  links.each(function(idx, el) {
-    $(el).find('img').css('border', '');
-  });
+  links.removeClass('nfe-follow');
+  links.removeClass('nfe-nofollow');
 }
 
 /*
